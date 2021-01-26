e3c6a2e8b20f53ede3dc48cde0d2f0cf68b4aa95
guang <542436458@qq.com>
2021-01-26 18:48:30 +0800

    记录列表添加筛选条件功能开始开发

--- src/pages/order/recordList/index.vue
+++ src/pages/order/recordList/index.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="order-record-list">
+    <van-dropdown-menu>
+      <van-dropdown-item :value="value1" :options="option1" />
+      <van-dropdown-item :value="value2" :options="option2" />
+    </van-dropdown-menu>
 
     <scroll-view
       v-if="dayList.length"
@@ -52,7 +56,19 @@ export default {
         limit: 5,
         page: 0
       },
-      timeInterval: timeInterval.DAY
+      timeInterval: timeInterval.DAY,
+      option1: [
+        { text: '全部商品', value: 0 },
+        { text: '新款商品', value: 1 },
+        { text: '活动商品', value: 2 }
+      ],
+      option2: [
+        { text: '默认排序', value: 'a' },
+        { text: '好评排序', value: 'b' },
+        { text: '销量排序', value: 'c' }
+      ],
+      value1: 0,
+      value2: 'a'
     }
   },
   onLoad (option) {
@@ -239,6 +255,6 @@ export default {
   }
 }
 .record-list {
-  height: 100vh;
+  height: 90vh;
 }
 </style>
